6f512826fecf5817e7453c8fb0efa66c1690d5db
Doguhan Uluca <duluca@gmail.com>
2014-03-09 20:19:07 -0400

    Simple controller and minimal settings

---
+++ OneApi/OneApi/App_Start/WebApiConfig.cs
@@ -0,0 +1,17 @@
+﻿namespace OneApi
+{
+    using System.Web.Http;
+
+    public static class WebApiConfig
+    {
+        public static void Register(HttpConfiguration config)
+        {
+            // Web API configuration and services
+            var formatters = config.Formatters;
+            formatters.Remove(formatters.XmlFormatter);
+
+            // Web API routes
+            config.MapHttpAttributeRoutes();
+        }
+    }
+}
---
+++ OneApi/OneApi/Controllers/ReservationController.cs
@@ -0,0 +1,40 @@
+﻿namespace OneApi.Controllers
+{
+    using System.Net;
+    using System.Net.Http;
+    using System.Collections.Generic;
+    using System.Web.Http;
+
+    [Authorize]
+    [RoutePrefix("reservation")]
+    public class ReservationController : ApiController
+    {
+        [HttpGet, Route("")]
+        public IEnumerable<string> Get()
+        {
+            return new[] { "value1", "value2" };
+        }
+
+        [HttpGet, Route("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        [HttpPost, Route("")]
+        public HttpResponseMessage Post([FromBody]string value)
+        {
+            return Request.CreateResponse(HttpStatusCode.Created, value);
+        }
+
+        [HttpPut, Route("{id}")]
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        [HttpDelete, Route("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
